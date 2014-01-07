29d9163724750c84dc4a3d2b37c213d04fe16286
Robert Kaye <rob@musicbrainz.org>
2014-01-07 10:29:49 -0800

    Added basic logstash files. UTF-8 doesn't work dues to a bug in logstash. We may need to wait for logstash 1.3.3

--- music-lager-server/recipes/default.rb
+++ music-lager-server/recipes/default.rb
@@ -1,3 +1,43 @@
+directory "/usr/local/logstash" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
+
+directory "/usr/local/logstash/jar" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
+
+directory "/usr/local/logstash/conf" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
+
+remote_file "/usr/local/logstash/jar/logstash-1.3.2-flatjar.jar" do
+  source "https://download.elasticsearch.org/logstash/logstash/logstash-1.3.2-flatjar.jar"
+  mode 00644
+end
+
+cookbook_file "/usr/local/logstash/conf/indexer.conf" do
+  source "indexer.conf"
+  group "root"
+  owner "root"
+  mode "0755"
+end
+
+cookbook_file "/usr/local/logstash/conf/shipper.conf" do
+  source "shipper.conf"
+  group "root"
+  owner "root"
+  mode "0755"
+end
+
 directory "/usr/local/music-lager-server" do
   owner "root"
   group "root"
